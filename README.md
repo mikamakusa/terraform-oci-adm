1eae785132994891366d1786cdb28ba05ac1e10d
mikamakusa <michael.dangleterre@gmail.com>
2024-08-20 08:59:31 +0200

    update

---
+++ README.md
@@ -0,0 +1,42 @@
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_oci"></a> [oci](#requirement\_oci) | >= 6.7.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_oci"></a> [oci](#provider\_oci) | >= 6.7.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_identity"></a> [identity](#module\_identity) | modules/terraform-oci-identity | n/a |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [oci_adm_knowledge_base.this](https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/adm_knowledge_base) | resource |
+| [oci_adm_remediation_recipe.this](https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/adm_remediation_recipe) | resource |
+| [oci_adm_remediation_run.this](https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/adm_remediation_run) | resource |
+| [oci_adm_vulnerability_audit.this](https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/adm_vulnerability_audit) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_compartment"></a> [compartment](#input\_compartment) | n/a | `any` | n/a | yes |
+| <a name="input_defined_tags"></a> [defined\_tags](#input\_defined\_tags) | n/a | `map(string)` | `[]` | no |
+| <a name="input_freeform_tags"></a> [freeform\_tags](#input\_freeform\_tags) | n/a | `map(string)` | `[]` | no |
+| <a name="input_knowledge_base"></a> [knowledge\_base](#input\_knowledge\_base) | n/a | <pre>list(object({<br>    id             = number<br>    compartment_id = any<br>    defined_tags   = optional(map(string))<br>    display_name   = optional(string)<br>    freeform_tags  = optional(map(string))<br>  }))</pre> | `[]` | no |
+| <a name="input_remediation_recipe"></a> [remediation\_recipe](#input\_remediation\_recipe) | n/a | <pre>list(object({<br>    compartment_id                = any<br>    is_run_triggered_on_kb_change = bool<br>    knowledge_base_id             = any<br>    display_name                  = optional(string)<br>    defined_tags                  = optional(map(string))<br>    freeform_tags                 = optional(map(string))<br>    detect_configuration = list(object({<br>      exclusions                   = optional(list(string))<br>      max_permissible_cvss_v2score = optional(number)<br>      max_permissible_cvss_v3score = optional(number)<br>      upgrade_policy               = optional(string)<br>    }))<br>    network_configuration = list(object({<br>      subnet_id = any<br>      nsg_ids   = list(any)<br>    }))<br>    scm_configuration = list(object({<br>      scm_type               = string<br>      branch                 = string<br>      is_automerge_enabled   = bool<br>      build_file_location    = optional(string)<br>      external_scm_type      = optional(string)<br>      oci_code_repository_id = optional(any)<br>      pat_secret_id          = optional(any)<br>      repository_url         = optional(string)<br>      username               = optional(string)<br>    }))<br>    verify_configuration = list(object({<br>      build_service_type    = string<br>      additional_parameters = optional(map(string))<br>      jenkins_url           = optional(string)<br>      job_name              = optional(any)<br>      pat_secret_id         = optional(any)<br>      pipeline_id           = optional(any)<br>      repository_url        = optional(string)<br>      trigger_secret_id     = optional(any)<br>      username              = optional(string)<br>      workflow_name         = optional(string)<br>    }))<br>  }))</pre> | `[]` | no |
+| <a name="input_remediation_run"></a> [remediation\_run](#input\_remediation\_run) | n/a | <pre>list(object({<br>    remediation_recipe_id = any<br>    defined_tags          = optional(map(string))<br>    display_name          = optional(string)<br>    freeform_tags         = optional(map(string))<br>  }))</pre> | `[]` | no |
+| <a name="input_vulnerability_audit"></a> [vulnerability\_audit](#input\_vulnerability\_audit) | n/a | <pre>list(object({<br>    knowledge_base_id = any<br>    defined_tags      = optional(map(string))<br>    display_name      = optional(string)<br>    freeform_tags     = optional(map(string))<br>    build_type        = optional(string)<br>    compartment_id    = optional(string)<br>    application_dependencies = optional(list(object({<br>      node_id                         = any<br>      application_dependency_node_ids = list(string)<br>      gav                             = optional(string)<br>      purl                            = optional(string)<br>    })))<br>    configuration = optional(list(object({<br>      exclusions                   = optional(list(string))<br>      max_permissible_cvss_v2score = optional(number)<br>      max_permissible_cvss_v3score = optional(number)<br>      max_permissible_severity     = optional(string)<br>    })))<br>    source = optional(list(object({<br>      type            = string<br>      description     = optional(string)<br>      oci_resource_id = optional(any)<br>    })))<br>    usage_data = optional(list(object({<br>      bucket      = string<br>      namespace   = string<br>      object      = string<br>      source_type = string<br>    })))<br>  }))</pre> | `[]` | no |
+
+## Outputs
+
+No outputs.
--- main.tf
+++ main.tf
@@ -8,25 +8,25 @@ resource "oci_adm_knowledge_base" "this" {
 
 resource "oci_adm_remediation_recipe" "this" {
   count                         = (length(var.compartment) && length(var.knowledge_base)) == 0 ? 0 : length(var.remediation_recipe)
-  compartment_id                = ""
-  is_run_triggered_on_kb_change = false
-  knowledge_base_id             = ""
-  display_name                  = ""
-  defined_tags                  = {}
-  freeform_tags                 = {}
+  compartment_id                = try(element(module.identity.*.compartment_id, lookup(var.remediation_recipe[count.index], "compartment_id")))
+  is_run_triggered_on_kb_change = lookup(var.remediation_recipe[count.index], "is_run_triggered_on_kb_change")
+  knowledge_base_id             = lookup(var.remediation_recipe[count.index], "knowledge_base_id")
+  display_name                  = lookup(var.remediation_recipe[count.index], "display_name")
+  defined_tags                  = merge(var.defined_tags, lookup(var.remediation_recipe[count.index], "defined_tags"))
+  freeform_tags                 = merge(var.freeform_tags, lookup(var.remediation_recipe[count.index], "freeform_tags"))
 
   dynamic "detect_configuration" {
-    for_each = ""
+    for_each = try(lookup(var.remediation_recipe[count.index], "detect_configuration") == null ? [] : ["detect_configuration"])
     content {
-      exclusions                   = []
-      max_permissible_cvss_v2score = 0
-      max_permissible_cvss_v3score = 0
-      upgrade_policy               = ""
+      exclusions                   = lookup(detect_configuration.value, "exclusions")
+      max_permissible_cvss_v2score = lookup(detect_configuration.value, "max_permissible_cvss_v2score")
+      max_permissible_cvss_v3score = lookup(detect_configuration.value, "max_permissible_cvss_v3score")
+      upgrade_policy               = lookup(detect_configuration.value, "upgrade_policy")
     }
   }
 
   dynamic "network_configuration" {
-    for_each = ""
+    for_each = try(lookup(var.remediation_recipe[count.index], "network_configuration") == null ? [] : ["network_configuration"])
     content {
       subnet_id = ""
       nsg_ids   = []
@@ -34,90 +34,90 @@ resource "oci_adm_remediation_recipe" "this" {
   }
 
   dynamic "scm_configuration" {
-    for_each = ""
+    for_each = try(lookup(var.remediation_recipe[count.index], "scm_configuration") == null ? [] : ["scm_configuration"])
     content {
-      scm_type               = ""
-      branch                 = ""
-      is_automerge_enabled   = true
-      build_file_location    = ""
-      external_scm_type      = ""
+      scm_type               = lookup(scm_configuration.value, "scm_type")
+      branch                 = lookup(scm_configuration.value, "branch")
+      is_automerge_enabled   = lookup(scm_configuration.value, "is_automerge_enabled")
+      build_file_location    = lookup(scm_configuration.value, "build_file_location")
+      external_scm_type      = lookup(scm_configuration.value, "external_scm_type")
       oci_code_repository_id = ""
       pat_secret_id          = ""
-      repository_url         = ""
-      username               = ""
+      repository_url         = lookup(scm_configuration.value, "repository_url")
+      username               = lookup(scm_configuration.value, "username")
     }
   }
 
   dynamic "verify_configuration" {
-    for_each = ""
+    for_each = try(lookup(var.remediation_recipe[count.index], "verify_configuration") == null ? [] : ["verify_configuration"])
     content {
-      build_service_type    = ""
-      additional_parameters = {}
-      jenkins_url           = ""
+      build_service_type    = lookup(verify_configuration.value, "build_service_type")
+      additional_parameters = lookup(verify_configuration.value, "additional_parameters")
+      jenkins_url           = lookup(verify_configuration.value, "jenkins_url")
       job_name              = ""
       pat_secret_id         = ""
       pipeline_id           = ""
-      repository_url        = ""
+      repository_url        = lookup(verify_configuration.value, "repository_url")
       trigger_secret_id     = ""
-      username              = ""
-      workflow_name         = ""
+      username              = lookup(verify_configuration.value, "username")
+      workflow_name         = lookup(verify_configuration.value, "workflow_name")
     }
   }
 }
 
 resource "oci_adm_remediation_run" "this" {
   count                 = length(var.remediation_recipe) == 0 ? 0 : length(var.remediation_run)
-  remediation_recipe_id = ""
-  defined_tags          = {}
-  display_name          = ""
-  freeform_tags         = {}
+  remediation_recipe_id = lookup(var.remediation_run[count.index], "remediation_recipe_id")
+  defined_tags          = merge(var.defined_tags, lookup(var.remediation_run[count.index], "defined_tags"))
+  display_name          = lookup(var.remediation_run[count.index], "display_name")
+  freeform_tags         = merge(var.freeform_tags, lookup(var.remediation_run[count.index], "freeform_tags"))
 }
 
 resource "oci_adm_vulnerability_audit" "this" {
-  count             = length(var.knowledge_base) == 0 ? 0 : length(var.knowledge_base)
-  knowledge_base_id = ""
-  defined_tags      = {}
-  display_name      = ""
-  freeform_tags     = {}
-  build_type        = ""
-  compartment_id    = ""
+  count             = length(var.knowledge_base) == 0 ? 0 : length(var.vulnerability_audit)
+  knowledge_base_id = try(element(oci_adm_knowledge_base.this.*.id, lookup(var.vulnerability_audit[count.index], "knowledge_base_id")))
+  defined_tags      = merge(var.defined_tags, lookup(var.vulnerability_audit[count.index], "defined_tags"))
+  display_name      = lookup(var.vulnerability_audit[count.index], "display_name")
+  freeform_tags     = merge(var.freeform_tags, lookup(var.vulnerability_audit[count.index], "freeform_tags"))
+  build_type        = lookup(var.vulnerability_audit[count.index], "build_type")
+  compartment_id    = try(element(module.identity.*.compartment_id, lookup(var.vulnerability_audit[count.index], "compartment_id")))
 
   dynamic "application_dependencies" {
-    for_each = ""
+    for_each = try(lookup(var.vulnerability_audit[count.index], "application_dependencies") == null ? [] : ["application_dependencies"])
     content {
       node_id                         = ""
-      application_dependency_node_ids = []
-      gav                             = ""
-      purl                            = ""
+      application_dependency_node_ids = lookup(application_dependencies.value, "application_dependency_node_ids")
+      gav                             = lookup(application_dependencies.value, "gav")
+      purl                            = lookup(application_dependencies.value, "purl")
     }
   }
 
   dynamic "configuration" {
-    for_each = ""
+    for_each = try(lookup(var.vulnerability_audit[count.index], "configuration") == null ? [] : ["configuration"])
     content {
-      exclusions = []
-      max_permissible_cvss_v2score = 0
-      max_permissible_cvss_v3score = 0
-      max_permissible_severity = ""
+      exclusions                   = lookup(configuration.value, "exclusions")
+      max_permissible_cvss_v2score = lookup(configuration.value, "max_permissible_cvss_v2score")
+      max_permissible_cvss_v3score = lookup(configuration.value, "max_permissible_cvss_v3score")
+      max_permissible_severity     = lookup(configuration.value, "max_permissible_severity")
     }
   }
 
   dynamic "source" {
-    for_each = ""
+    for_each = try(lookup(var.vulnerability_audit[count.index], "source") == null ? [] : ["source"])
     content {
-      type = ""
-      description = ""
+      type            = lookup(source.value, "type")
+      description     = lookup(source.value, "description")
       oci_resource_id = ""
     }
   }
 
   dynamic "usage_data" {
-    for_each = ""
+    for_each = try(lookup(var.vulnerability_audit[count.index], "usage_data") == null ? [] : ["usage_data"])
     content {
-      bucket      = ""
-      namespace   = ""
-      object      = ""
-      source_type = ""
+      bucket      = lookup(usage_data.value, "bucket")
+      namespace   = lookup(usage_data.value, "namespace")
+      object      = lookup(usage_data.value, "object")
+      source_type = lookup(usage_data.value, "source_type")
     }
   }
 }
